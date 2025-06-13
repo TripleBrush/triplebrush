8f06601e383cbf8e65f3a249dca8cc2fa3fa8856
TripleBrush <triplebrushed@gmail.com>
2025-06-12 22:20:23 -0400

    Update rss.xml.js

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -5,9 +5,9 @@ import { SITE_TITLE, SITE_DESCRIPTION } from "../consts";
 export async function GET(context) {
   const posts = await getCollection("blog");
   return rss({
-    title: Triplebrush,
+    title: 'triplebrush.tv',
     description: SITE_DESCRIPTION,
-    site: triplebrush.tv,
+    site: 'triplebrush.tv',
     items: posts.map((post) => ({
       ...post.data,
       link: `/blog/${post.id}/`,
